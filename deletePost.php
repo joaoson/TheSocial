d40cc75e7b5221544580a56310710f4b1ce5e30d
Joao Freitas <joaovitor6586@gmail.com>
2023-11-19 21:42:03 -0300

    Fixed unset

--- deletePost.php
+++ deletePost.php
@@ -5,12 +5,13 @@
     if (!isset($_SESSION["id"])) {
         header("Location: index.php");
     }
+    $id = $_SESSION["id"];
+    $_SESSION["id"] = $id;
 
     
         $idPost = htmlspecialchars($_GET["idPost"]);
         $sql = "DELETE FROM Post WHERE id_post = $idPost";
         $result = $connection->query($sql);
-        session_unset();
 
         if ($result === TRUE) {
 ?>
